2e048cc3f019e2a5fbcaa526184d7be59600b326
auyu0408 <datealive102164@gmail.com>
2021-01-21 21:43:46 +0800

    commit problem12

--- medium/6. ZigZag Conversion/6.ZigZag_Conversion.c
+++ medium/6. ZigZag Conversion/6.ZigZag_Conversion.c
@@ -3,23 +3,17 @@
 #include<string.h>
 
 char * convert(char * s, int numRows){
-    int size=numRows*2-2,len=strlen(s);
+    int size=numRows==1?1:(numRows*2-2);
     int now=0,row=0,space=size-row;
-    char *a=(char *)malloc(sizeof(char)*1000);
-    if (numRows==1) size=1;
+    char *a=(char *)malloc(sizeof(char)*1000);//用malloc可以存在到程式結束
     for(int i=0;i<strlen(s);)
     {
         a[i]=s[now];
-        if(row==numRows-1||row==0)
+        if(row!=numRows-1&&row!=0)
         {
             i++;
-            now+=size;
-        }
-        else
-        {
-            if((space)>=strlen(s))
+            if((space)>=strlen(s))//若過大就重置
             {
-                i++;
                 row++;
                 now=row;
                 space=size-row;
@@ -27,13 +21,12 @@ char * convert(char * s, int numRows){
             }
             else
             {
-                i++;
-                a[i]=s[space];
-                space+=size;
+                a[i]=s[space];//非第一排或最後一排，會有斜上角要處理
+                space+=size;//同一輪內斜上角差距是size-行數，每組斜上角間距也是size
             }
-            i++;
-            now+=size;
         }
+        i++;
+        now+=size;//每組間距是size
         if(now>=strlen(s))
         {
             row++;
@@ -41,7 +34,7 @@ char * convert(char * s, int numRows){
             space=size-row;
         }
     }
-    a[strlen(s)]='\0';
+    a[strlen(s)]='\0';//最後放入字串結尾(因為不知道是一個個輸出還是直接輸出字串)
     return a;
 }
 int main()
